8893e76d9d618fe395d28a087b51a8b1e304754b
SAMKE-glitch <sammythemwa@gmail.com>
2023-12-09 01:42:47 +0300

    add floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function add that takes a float
+a and a float b as arguments and returns
+their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and returns a float"""
+    return (a + b)
